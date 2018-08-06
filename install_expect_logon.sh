da02bcd44df68175d6e0a527deeff909677d4499
root <root@axe3.c.dauntless-bay-211806.internal>
2018-08-06 04:54:07 +0000

    xxx

--- install_expect_logon.sh
+++ install_expect_logon.sh
@@ -3,7 +3,9 @@ yum list installed | grep expect || yum install -y expect
 wget http://axe3.tk/tools/linux/expect_logon/axe_v2.sh
 wget http://axe3.tk/tools/linux/expect_logon/222.sh
 wget http://axe3.tk/tools/linux/expect_logon/15.sh
+wget http://axe3.tk/tools/linux/expect_logon/17.sh
 mv axe_v2.sh /usr/bin/axe_v2
 mv 222.sh /usr/bin/222
 mv 15.sh /usr/bin/15
-chmod +x /usr/bin/axe_v2 /usr/bin/222 /usr/bin/15
+mv 17.sh /usr/bin/17
+chmod +x /usr/bin/axe_v2 /usr/bin/222 /usr/bin/15 /usr/bin/15
